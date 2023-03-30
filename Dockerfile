145ed2669a08572f19ba5e0e0684d35cec88544f
Abhinav Dhiman <dhiman.ahnv@gmail.com>
2023-03-31 03:46:14 +0530

    chore: updated script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM alpine:3 as app
 
-RUN apk add --no-cache curl git unzip
+RUN apk add --no-cache curl git unzip jq bash
 
 COPY --chmod=0755 ./stripper.sh /strip/stripper.sh
 COPY --chmod=0755 ./stripper.sh /etc/periodic/daily/strip
 
-CMD ["sh", "-c", "/strip/stripper.sh && crond -f -l 2"]
+CMD ["bash", "-c", "/strip/stripper.sh && crond -f -l 2"]
--- stripper.sh
+++ stripper.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-rm -rf /tmp/stripdance
-git clone $STRIP_REPO /tmp/stripdance
-cd /tmp/stripdance
+rm -rf /tmp/stripcharts
+git clone $STRIP_REPO /tmp/stripcharts
+cd /tmp/stripcharts
 
-rm -rf sexy
+rm -rf enterprise
+rm -rf dependency
 rm -rf stable
 rm -rf incubator
 mkdir stable
 mkdir incubator
+mkdir dependency
+mkdir enterprise
+
+cd ../
 
 curl -L -o main.zip https://github.com/truecharts/catalog/archive/refs/heads/main.zip
 
@@ -20,32 +25,78 @@ ls -la .
 
 rm -rf .github
 
-cd stable
-ls -1 . |
-grep -E "^($STRIP_STABLE)\$" |
-xargs cp -r -t ../../stable/
+if [ -n "$STRIP_STABLE" ]; then
+  cd stable
+  ls -1 . | grep -E "^($STRIP_STABLE)\$" | xargs cp -r -t ../../stripcharts/stable
+  cd ..
+fi
 
-cd ../incubator
-ls -1 . |
-grep -E "^($STRIP_INCUBATOR)\$" |
-xargs cp -r -t ../../incubator/
+if [ -n "$STRIP_INCUBATOR" ]; then
+  cd incubator
+  ls -1 . | grep -E "^($STRIP_INCUBATOR)\$" | xargs cp -r -t ../../stripcharts/incubator/
+  cd ..
+fi
 
-cd ..
+if [ -n "$STRIP_DEPENDENCY" ]; then
+  cd dependency
+  ls -1 . | grep -E "^($STRIP_DEPENDENCY)\$" | xargs cp -r -t ../../stripcharts/dependency/
+  cd ..
+fi
 
-rm -rf dependency enterprise incubator stable
+if [ -n "$STRIP_ENTERPRISE" ]; then
+  cd enterprise
+  ls -1 . | grep -E "^($STRIP_ENTERPRISE)\$" | xargs cp -r -t ../../stripcharts/enterprise/
+  cd ..
+fi
 
-for x in ./* ./.[!.]* ./..?*; do
-  if [ -e "$x" ]; then mv -- "$x" ../; fi
+cp catalog.json ../stripcharts/catalog.json
+
+cd ../stripcharts
+
+STABLE_APPS=(`echo $STRIP_STABLE | tr '|' ' '`)
+STABLE_MAPPING=""
+
+for i in "${STABLE_APPS[@]}"
+do
+  STABLE_MAPPING="$STABLE_MAPPING $i: .stable.$i,"
 done
+
+INCUBATOR_APPS=(`echo $STRIP_INCUBATOR | tr '|' ' '`)
+INCUBATOR_MAPPING=""
+
+for i in "${INCUBATOR_APPS[@]}"
+do
+  INCUBATOR_MAPPING="$INCUBATOR_MAPPING $i: .incubator.$i,"
+done
+
+DEPENDENCY_APPS=(`echo $STRIP_DEPENDENCY | tr '|' ' '`)
+DEPENDENCY_MAPPING=""
+
+for i in "${DEPENDENCY_APPS[@]}"
+do
+  DEPENDENCY_MAPPING="$DEPENDENCY_MAPPING $i: .dependency.$i,"
+done
+
+ENTERPRISE_APPS=(`echo $STRIP_ENTERPRISE | tr '|' ' '`)
+ENTERPRISE_MAPPING=""
+for i in "${ENTERPRISE_APPS[@]}"
+do
+  ENTERPRISE_MAPPING="$ENTERPRISE_MAPPING $i: .enterprise.$i,"
+done
+
+jq "{charts, test, stable: { $STABLE_MAPPING }, incubator: { $INCUBATOR_MAPPING }, dependency: { $DEPENDENCY_MAPPING }, enterprise: { $ENTERPRISE_MAPPING }}" catalog.json > catalog.json.tmp
+mv catalog.json.tmp catalog.json
 
 cd ..
 rm -rf catalog-main
 
+cd stripcharts
+
 git add .
-git config --global user.email "you@example.com"
-git config --global user.name "Sexy Stripper"
+git config --global user.email "${GITHUB_USERNAME:=strip@charts.com}"
+git config --global user.name "${GITHUB_USERNAME:=StripCharts}"
 git commit -a --amend -m "update" || git commit -a -m "update"
 git push -f
 
-cd /tmp
-rm -rf /tmp/stripdance
+cd ../../
+rm -rf /tmp/stripcharts
