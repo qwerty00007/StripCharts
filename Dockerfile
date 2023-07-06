c3e1960bb51f542b945845bb069d7e061b708957
Abhinav Dhiman <dhiman.ahnv@gmail.com>
2023-07-06 17:15:54 +0530

    fix: updated image

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM alpine:3 as app
+FROM debian:12 as app
 
-RUN apk add --no-cache curl git unzip jq bash
+RUN apt update && apt install curl git unzip jq bash -y
 
 COPY --chmod=0755 ./stripper.sh /strip/stripper.sh
 COPY --chmod=0755 ./stripper.sh /etc/periodic/daily/strip
--- stripper.sh
+++ stripper.sh
@@ -1,107 +1,101 @@
 #!/bin/bash
 
-rm -rf /tmp/stripcharts
-git clone $STRIP_REPO /tmp/stripcharts
-cd /tmp/stripcharts
+TEMP_FOLDER="/tmp"
+TEMP_CHARTS_FOLDER="$TEMP_FOLDER/stripcharts"
+TEMP_CATALOG_FOLDER="$TEMP_FOLDER/catalog"
 
-rm -rf enterprise
-rm -rf dependency
-rm -rf stable
-rm -rf incubator
-mkdir stable
-mkdir incubator
-mkdir dependency
-mkdir enterprise
+initial_setup() {
 
-cd ../
+  if [ -d "$TEMP_CHARTS_FOLDER" ]; then
+    cd $TEMP_CHARTS_FOLDER
+    git clean -f -x
+    git fetch origin main
+    git reset --hard origin/main
+    git pull
+  else
+    git clone $STRIP_REPO $TEMP_CHARTS_FOLDER
+  fi
 
-curl -L -o main.zip https://github.com/truecharts/catalog/archive/refs/heads/main.zip
+  if [ -d "$TEMP_CATALOG_FOLDER" ]; then
+    cd $TEMP_CATALOG_FOLDER
+    git clean -f -x
+    git fetch origin main
+    git reset --hard origin/main
+    git pull
+  else
+    git clone -b main --single-branch https://github.com/truecharts/catalog $TEMP_CATALOG_FOLDER
+  fi
 
-unzip main.zip
-rm -f main.zip
-cd catalog-main
+  cd $TEMP_FOLDER
+}
 
-ls -la .
 
-rm -rf .github
+cleanup_strip_charts() {
+  cd $TEMP_CHARTS_FOLDER
+  rm -rf enterprise
+  rm -rf dependency
+  rm -rf stable
+  rm -rf incubator
+  mkdir stable
+  mkdir incubator
+  mkdir dependency
+  mkdir enterprise
 
-if [ -n "$STRIP_STABLE" ]; then
-  cd stable
-  echo $STRIP_STABLE
-  ls -1 . | grep -E "^($STRIP_STABLE)\$" | xargs cp -r -t ../../stripcharts/stable
-  cd ..
-fi
+  cd $TEMP_FOLDER
+}
 
-if [ -n "$STRIP_INCUBATOR" ]; then
-  cd incubator
-  echo $STRIP_INCUBATOR
-  ls -1 . | grep -E "^($STRIP_INCUBATOR)\$" | xargs cp -r -t ../../stripcharts/incubator/
-  cd ..
-fi
+copy_charts() {
+  if [ -n "$2" ]; then
+    cd $TEMP_CATALOG_FOLDER/$1
+    echo "Copying charts for $1: $2"
+    ls -1 . | grep -E "^($2)\$" | xargs cp -r -t $TEMP_CHARTS_FOLDER/$1
+    cd ..
+  fi
+}
 
-if [ -n "$STRIP_DEPENDENCY" ]; then
-  cd dependency
-  echo $STRIP_DEPENDENCY
-  ls -1 . | grep -E "^($STRIP_DEPENDENCY)\$" | xargs cp -r -t ../../stripcharts/dependency/
-  cd ..
-fi
+parse_mapping() {
+  if [ -n "$2" ]; then
+    APPS=$(echo $2 | tr '|' ' ')
+    MAPPING=""
 
-if [ -n "$STRIP_ENTERPRISE" ]; then
-  cd enterprise
-  echo $STRIP_ENTERPRISE
-  ls -1 . | grep -E "^($STRIP_ENTERPRISE)\$" | xargs cp -r -t ../../stripcharts/enterprise/
-  cd ..
-fi
+    for i in $APPS
+    do
+      MAPPING="$MAPPING \"$i\": .$1.\"$i\","
+    done
 
-cp catalog.json ../stripcharts/catalog.json
+    echo ${MAPPING}
+  fi
+}
 
-cd ../stripcharts
-ls -al .
+copy_catalog_file() {
+  cp $TEMP_CATALOG_FOLDER/catalog.json $TEMP_CHARTS_FOLDER/catalog.json
+  
+  cd $TEMP_CHARTS_FOLDER
+  ls -al .
 
-STABLE_APPS=(`echo $STRIP_STABLE | tr '|' ' '`)
-STABLE_MAPPING=""
+  local STABLE_MAPPING=$(parse_mapping stable $STRIP_STABLE )
+  local INCUBATOR_MAPPING=$(parse_mapping incubator $STRIP_INCUBATOR )
+  local DEPENDENCY_MAPPING=$(parse_mapping dependency $STRIP_DEPENDENCY )
+  local ENTERPRISE_MAPPING=$(parse_mapping enterprise $STRIP_ENTERPRISE )
 
-for i in "${STABLE_APPS[@]}"
-do
-  STABLE_MAPPING="$STABLE_MAPPING $i: .stable.$i,"
-done
+  jq "{charts, test, stable: { $STABLE_MAPPING },  incubator: { $INCUBATOR_MAPPING }, dependency: { $DEPENDENCY_MAPPING }, enterprise: { $ENTERPRISE_MAPPING }}" catalog.json > catalog.json.tmp
+  mv catalog.json.tmp catalog.json
+}
 
-INCUBATOR_APPS=(`echo $STRIP_INCUBATOR | tr '|' ' '`)
-INCUBATOR_MAPPING=""
+commit_changes() {
+  cd $TEMP_CHARTS_FOLDER
+  git add .
+  git config --global user.email "${GITHUB_EMAIL:=strip@charts.com}"
+  git config --global user.name "${GITHUB_NAME:=StripCharts}"
+  git commit -a -m "update"
+  git push -f
+}
 
-for i in "${INCUBATOR_APPS[@]}"
-do
-  INCUBATOR_MAPPING="$INCUBATOR_MAPPING $i: .incubator.$i,"
-done
-
-DEPENDENCY_APPS=(`echo $STRIP_DEPENDENCY | tr '|' ' '`)
-DEPENDENCY_MAPPING=""
-
-for i in "${DEPENDENCY_APPS[@]}"
-do
-  DEPENDENCY_MAPPING="$DEPENDENCY_MAPPING $i: .dependency.$i,"
-done
-
-ENTERPRISE_APPS=(`echo $STRIP_ENTERPRISE | tr '|' ' '`)
-ENTERPRISE_MAPPING=""
-for i in "${ENTERPRISE_APPS[@]}"
-do
-  ENTERPRISE_MAPPING="$ENTERPRISE_MAPPING $i: .enterprise.$i,"
-done
-
-jq "{charts, test, stable: { $STABLE_MAPPING }, incubator: { $INCUBATOR_MAPPING }, dependency: { $DEPENDENCY_MAPPING }, enterprise: { $ENTERPRISE_MAPPING }}" catalog.json > catalog.json.tmp
-mv catalog.json.tmp catalog.json
-
-cd ..
-rm -rf catalog-main
-
-cd stripcharts
-
-git add .
-git config --global user.email "${GITHUB_EMAIL:=strip@charts.com}"
-git config --global user.name "${GITHUB_NAME:=StripCharts}"
-git commit -a -m "update"
-git push -f
-
-cd ../../
-rm -rf /tmp/stripcharts
+initial_setup
+cleanup_strip_charts
+copy_charts stable $STRIP_STABLE
+copy_charts incubator $STRIP_INCUBATOR
+copy_charts dependency $STRIP_DEPENDENCY
+copy_charts enterprise $STRIP_ENTERPRISE
+copy_catalog_file
+commit_changes
